a43a024258c5bfeab54f8ab8a70ec29e09430c78
Hiroshi Murakami <piroz@users.noreply.github.com>
2017-07-24 16:39:05 +0900

    browsersync travis

---
+++ bs-config.js
@@ -0,0 +1,119 @@
+const os = require("os");
+
+var googlechrome;
+
+if(os.type().toString().match("Windows")) {
+    googlechrome = "chrome.exe";
+} else {
+    googlechrome = "google chrome";
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    "ui": {
+        "port": 3000,
+        "weinre": {
+            "port": 3001
+        }
+    },
+    "files": [
+        "./dist/**/*"
+    ],
+    "watchOptions": {},
+    "server": {
+        "baseDir": "./dist/",
+        "middleware": (req, res, next) => {
+            const timestamp = `[${new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')}]`;
+            console.log(`${timestamp} <${req.method}> ${req.originalUrl} - ${req.connection.remoteAddress} - ${req.headers['user-agent']}`);
+            next();
+        }
+    },
+    "proxy": false,
+    "port": 9999,
+    "middleware": false,
+    "serveStatic": [],
+    "ghostMode": {
+        "clicks": true,
+        "scroll": true,
+        "forms": {
+            "submit": true,
+            "inputs": true,
+            "toggles": true
+        }
+    },
+    "logLevel": "info",
+    "logPrefix": "IMG-PRV",
+    "logConnections": false,
+    "logFileChanges": true,
+    "logSnippet": true,
+    "rewriteRules": [],
+    "open": "local",
+    "browser": [googlechrome],
+    "cors": false,
+    "xip": false,
+    "hostnameSuffix": false,
+    "reloadOnRestart": false,
+    "notify": true,
+    "scrollProportionally": true,
+    "scrollThrottle": 0,
+    "scrollRestoreTechnique": "window.name",
+    "scrollElements": [],
+    "scrollElementMapping": [],
+    "reloadDelay": 0,
+    "reloadDebounce": 0,
+    "reloadThrottle": 100,
+    "plugins": [],
+    "injectChanges": true,
+    "startPath": null,
+    "minify": true,
+    "host": null,
+    "localOnly": false,
+    "codeSync": true,
+    "timestamps": true,
+    "clientEvents": [
+        "scroll",
+        "scroll:element",
+        "input:text",
+        "input:toggles",
+        "form:submit",
+        "form:reset",
+        "click"
+    ],
+    "socket": {
+        "socketIoOptions": {
+            "log": false
+        },
+        "socketIoClientConfig": {
+            "reconnectionAttempts": 50
+        },
+        "path": "/browser-sync/socket.io",
+        "clientPath": "/browser-sync",
+        "namespace": "/browser-sync",
+        "clients": {
+            "heartbeatTimeout": 5000
+        }
+    },
+    "tagNames": {
+        "less": "link",
+        "scss": "link",
+        "css": "link",
+        "jpg": "img",
+        "jpeg": "img",
+        "png": "img",
+        "svg": "img",
+        "gif": "img",
+        "js": "script"
+    }
+};
